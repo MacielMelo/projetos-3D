0e97d52bffd7a37f1a112229a1c445ad36d69340
Maciel Melo <maciel.melo@grupoboticario.com.br>
2025-12-15 09:02:03 -0300

    add: tag cubo

---
+++ tag_cubo.py
@@ -0,0 +1,38 @@
+import cadquery as cq
+
+# Parâmetros
+altura = 20      # z
+largura = 20     # x
+espessura = 20   # y
+diametro_circulo = 15.5
+profundidade_circulo = 2.5
+raio_arredondamento = 4
+
+# Criar cubo com arestas arredondadas
+resultado = (cq.Workplane("XY")
+    .box(largura, espessura, altura)
+    .edges()
+    .fillet(raio_arredondamento)
+)
+
+# Adicionar círculo em cada face
+# Face superior (Z+)
+resultado = resultado.faces(">Z").workplane().circle(diametro_circulo/2).cutBlind(-profundidade_circulo)
+
+# Face inferior (Z-)
+resultado = resultado.faces("<Z").workplane().circle(diametro_circulo/2).cutBlind(-profundidade_circulo)
+
+# Face frontal (Y+)
+resultado = resultado.faces(">Y").workplane(centerOption="CenterOfBoundBox").circle(diametro_circulo/2).cutBlind(-profundidade_circulo)
+
+# Face traseira (Y-)
+resultado = resultado.faces("<Y").workplane(centerOption="CenterOfBoundBox").circle(diametro_circulo/2).cutBlind(-profundidade_circulo)
+
+# Face direita (X+)
+resultado = resultado.faces(">X").workplane(centerOption="CenterOfBoundBox").circle(diametro_circulo/2).cutBlind(-profundidade_circulo)
+
+# Face esquerda (X-)
+resultado = resultado.faces("<X").workplane(centerOption="CenterOfBoundBox").circle(diametro_circulo/2).cutBlind(-profundidade_circulo)
+
+# Exportar para STL
+cq.exporters.export(resultado, f"./stl/tag_cubo_{altura}.stl")
